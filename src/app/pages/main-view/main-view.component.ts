98dce787b5de4d6a3559c855476f73f7060132a5
Alba Galvez <alba.galvez@autotrader.co.uk>
2021-03-03 18:13:29 +0000

    created board and tasks UI

---
+++ src/app/pages/main-view/main-view.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
+
+@Component({
+  selector: 'app-main-view',
+  templateUrl: './main-view.component.html',
+  styleUrls: ['./main-view.component.scss'],
+})
+export class MainViewComponent implements OnInit {
+  constructor() {}
+  todo = ['Work', 'Run', 'Call mum', 'Book appointment'];
+  doing = ['Docker tutorial'];
+  ideas = ['Kanban board in Angular'];
+  done = ['Get up', 'How to', 'Laundry', 'Message Susana'];
+
+  ngOnInit(): void {}
+
+  // tslint:disable-next-line: typedef
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
+}
